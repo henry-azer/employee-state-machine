c28490810d64c6a88aada9c993fff2472bb06c33
Henry-Azer <henryazer@outlook.com>
2023-06-17 23:08:22 +0300

    initial commit

---
+++ src/main/java/com/henry/employeestatemachine/EmployeeStateMachineApplication.java
@@ -0,0 +1,13 @@
+package com.henry.employeestatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeStateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henry/employeestatemachine/EmployeeStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.employeestatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
