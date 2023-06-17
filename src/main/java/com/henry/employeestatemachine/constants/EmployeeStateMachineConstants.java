97c44c109a3c815d01bf928824608c9ca3a24ce7
Henry-Azer <henryazer@outlook.com>
2023-06-18 00:28:21 +0300

    implement employee state machine constants

---
+++ src/main/java/com/henry/employeestatemachine/constants/EmployeeStateMachineConstants.java
@@ -0,0 +1,10 @@
+package com.henry.employeestatemachine.constants;
+
+/**
+ * @author Henry Azer
+ * @since 17/06/2023
+ */
+public interface EmployeeStateMachineConstants {
+    String EVENT = "event";
+    String EMPLOYEE_ID = "employeeId";
+}
